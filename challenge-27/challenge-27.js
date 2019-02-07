4a5f8ac418423d3e57b5506d5183179f5e870ff5
ismael sidney <ismaelsidney@gmail.com>
2019-02-06 21:02:29 -0300

    challenge-27

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -19,3 +19,84 @@ Crie os seguintes métodos para verificação de tipo:
 - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
+(function(){
+  'use strict';
+function DOM(elements) {
+  this.element = document.querySelectorAll(elements);
+}
+
+DOM.prototype.on = function on(eventType,callback){
+  Array.prototype.forEach.call(this.element,function(element) {
+    element.addEventListener(eventType,callback,false);
+  });
+};
+
+DOM.prototype.off = function off(eventType,callback){
+  Array.prototype.forEach.call(this.element,function(element) {
+    element.removeEventListener(eventType,callback,false);
+  });
+};
+
+DOM.prototype.get = function get(){
+  return this.element;
+};
+
+DOM.prototype.forEach = function forEach(){
+  return Array.prototype.forEach.apply(this.element,arguments);
+};
+
+DOM.prototype.map = function map(){
+  return Array.prototype.map.apply(this.element,arguments)
+}
+
+DOM.prototype.filter = function filter(){
+  return Array.prototype.filter.apply(this.element,arguments)
+}
+
+DOM.prototype.reduce = function reduce(){
+  return Array.prototype.reduce.apply(this.element,arguments)
+}
+
+DOM.prototype.reduceRight = function reduceRight(){
+  return Array.prototype.reduceRight.apply(this.element,arguments)
+}
+
+DOM.prototype.every = function every(){
+  return Array.prototype.every.apply(this.element,arguments)
+}
+
+DOM.prototype.some = function some(){
+  return Array.prototype.some.apply(this.element,arguments)
+}
+
+DOM.prototype.isArray = function isArray (paran) {
+  return Object.prototype.toString.call(paran)=== '[object Array]';
+}
+DOM.prototype.isObject = function isObject (paran) {
+  return Object.prototype.toString.call(paran)=== '[object Object]';
+}
+DOM.prototype.isFunction = function isFunction (paran) {
+  return Object.prototype.toString.call(paran)=== '[object Function]';
+}
+DOM.prototype.isNumber = function isNumber (paran) {
+  return Object.prototype.toString.call(paran)=== '[object Number]';
+}
+DOM.prototype.isString = function isString (paran) {
+  return Object.prototype.toString.call(paran)=== '[object String]';
+}
+DOM.prototype.isBoolean = function isBoolean (paran) {
+  return Object.prototype.toString.call(paran)=== '[object Boolean]';
+}
+DOM.prototype.isNull = function isNull (paran) {
+  return Object.prototype.toString.call(paran)=== '[object Null]' ||
+  Object.prototype.toString.call(paran)=== '[object Undefined]';
+}
+//isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
+/*var $a = new DOM('[data-js="link"]');
+  console.log($a);
+  var mapItem=$a.map(function(item){
+    return item.getAttribute('data-js')
+  });
+console.log(mapItem);*/
+console.log(DOM.prototype.isBoolean(true));
+})();
