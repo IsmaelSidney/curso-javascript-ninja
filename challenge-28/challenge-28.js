bd30d0a80416c149761b7750c1ff204c59df7bd9
ismael sidney <ismaelsidney@gmail.com>
2019-03-05 17:51:00 -0300

    challenge-28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,102 +1,34 @@
-(function () {
-    'use strict';
-    /*
-    No HTML:
-    - Crie um formulário com um input de texto que receberá um CEP e um botão
-    de submit;
-    - Crie uma estrutura HTML para receber informações de endereço:
-    "Logradouro, Bairro, Estado, Cidade e CEP." Essas informações serão
-    preenchidas com os dados da requisição feita no JS.
-    - Crie uma área que receberá mensagens com o status da requisição:
-    "Carregando, sucesso ou erro."
+(function (DOM) {
+  'use strict';
+  /*
+  No HTML:
+  - Crie um formulário com um input de texto que receberá um CEP e um botão
+  de submit;
+  - Crie uma estrutura HTML para receber informações de endereço:
+  "Logradouro, Bairro, Estado, Cidade e CEP." Essas informações serão
+  preenchidas com os dados da requisição feita no JS.
+  - Crie uma área que receberá mensagens com o status da requisição:
+  "Carregando, sucesso ou erro."
 
-    No JS:
-    - O CEP pode ser entrado pelo usuário com qualquer tipo de caractere, mas
-    deve ser limpo e enviado somente os números para a requisição abaixo;
-    - Ao submeter esse formulário, deve ser feito um request Ajax para a URL:
-    "https://viacep.com.br/ws/[CEP]/json/", onde [CEP] será o CEP passado
-    no input criado no HTML;
-    - Essa requisição trará dados de um CEP em JSON. Preencha campos na tela
-    com os dados recebidos.
-    - Enquanto os dados são buscados, na área de mensagens de status, deve mostrar
-    a mensagem: "Buscando informações para o CEP [CEP]..."
-    - Se não houver dados para o CEP entrado, mostrar a mensagem:
-    "Não encontramos o endereço para o CEP [CEP]."
-    - Se houver endereço para o CEP digitado, mostre a mensagem:
-    "Endereço referente ao CEP [CEP]:"
-    - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
-    adicionar as informações em tela.
-    */
-    function DOM(elements) {
-      this.element = document.querySelectorAll(elements);
-    }
+  No JS:
+  - O CEP pode ser entrado pelo usuário com qualquer tipo de caractere, mas
+  deve ser limpo e enviado somente os números para a requisição abaixo;
+  - Ao submeter esse formulário, deve ser feito um request Ajax para a URL:
+  "https://viacep.com.br/ws/[CEP]/json/", onde [CEP] será o CEP passado
+  no input criado no HTML;
+  - Essa requisição trará dados de um CEP em JSON. Preencha campos na tela
+  com os dados recebidos.
+  - Enquanto os dados são buscados, na área de mensagens de status, deve mostrar
+  a mensagem: "Buscando informações para o CEP [CEP]..."
+  - Se não houver dados para o CEP entrado, mostrar a mensagem:
+  "Não encontramos o endereço para o CEP [CEP]."
+  - Se houver endereço para o CEP digitado, mostre a mensagem:
+  "Endereço referente ao CEP [CEP]:"
+  - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
+  adicionar as informações em tela.
+  */
 
-    DOM.prototype.on = function on(eventType, callback) {
-      Array.prototype.forEach.call(this.element, function (element) {
-        element.addEventListener(eventType, callback, false);
-      });
-    };
-
-    DOM.prototype.off = function off(eventType, callback) {
-      Array.prototype.forEach.call(this.element, function (element) {
-        element.removeEventListener(eventType, callback, false);
-      });
-    };
-
-    DOM.prototype.get = function get() {
-      return this.element;
-    };
-
-    DOM.prototype.forEach = function forEach() {
-      return Array.prototype.forEach.apply(this.element, arguments);
-    };
-
-    DOM.prototype.map = function map() {
-      return Array.prototype.map.apply(this.element, arguments)
-    }
-
-    DOM.prototype.filter = function filter() {
-      return Array.prototype.filter.apply(this.element, arguments)
-    }
-
-    DOM.prototype.reduce = function reduce() {
-      return Array.prototype.reduce.apply(this.element, arguments)
-    }
-
-    DOM.prototype.reduceRight = function reduceRight() {
-      return Array.prototype.reduceRight.apply(this.element, arguments)
-    }
-
-    DOM.prototype.every = function every() {
-      return Array.prototype.every.apply(this.element, arguments)
-    }
-
-    DOM.prototype.some = function some() {
-      return Array.prototype.some.apply(this.element, arguments)
-    }
-
-    DOM.prototype.isArray = function isArray(paran) {
-      return Object.prototype.toString.call(paran) === '[object Array]';
-    }
-    DOM.prototype.isObject = function isObject(paran) {
-      return Object.prototype.toString.call(paran) === '[object Object]';
-    }
-    DOM.prototype.isFunction = function isFunction(paran) {
-      return Object.prototype.toString.call(paran) === '[object Function]';
-    }
-    DOM.prototype.isNumber = function isNumber(paran) {
-      return Object.prototype.toString.call(paran) === '[object Number]';
-    }
-    DOM.prototype.isString = function isString(paran) {
-      return Object.prototype.toString.call(paran) === '[object String]';
-    }
-    DOM.prototype.isBoolean = function isBoolean(paran) {
-      return Object.prototype.toString.call(paran) === '[object Boolean]';
-    }
-    DOM.prototype.isNull = function isNull(paran) {
-      return Object.prototype.toString.call(paran) === '[object Null]' ||
-        Object.prototype.toString.call(paran) === '[object Undefined]';
-    }
+  function app() {
     var $formCep = new DOM('[data-js="form-cep"]');
     var $inputCep = new DOM('[data-js="input-cep"]');
     var $logradouro = new DOM('[data-js="logradouro"]');
@@ -107,7 +39,6 @@
     var $status = new DOM('[data-js="status"]');
     var ajax = new XMLHttpRequest();
     $formCep.on('submit', handleSubmitFormCep);
-
 
     function handleSubmitFormCep(event) {
       event.preventDefault();
@@ -122,32 +53,45 @@
 
     function handleReadyStateChange() {
       if (isRequestok()) {
-        fillCepFields();
         getMessage('ok');
+        fillCepFields();
       }
     }
 
     function fillCepFields() {
       var data = parseData();
-      if (!data)
-      getMessage('error');
-      console.log('DATA', data);
-      $logradouro.get()[0].textContent = data.logradouro;
-      $bairro.get()[0].textContent = data.bairro;
-      $cidade.get()[0].textContent = data.localidade;
-      $estado.get()[0].textContent = data.uf;
-      $cep.get()[0].textContent = data.cep;
+      if (data.status === 0) {
+        getMessage('error');
+        data = clearData();
+      }
+
+      $logradouro.get()[0].textContent = data.address;
+      $bairro.get()[0].textContent = data.district;
+      $cidade.get()[0].textContent = data.city;
+      $estado.get()[0].textContent = data.state;
+      $cep.get()[0].textContent = data.code;
+    }
+
+    function clearData() {
+      return {
+        address: '-',
+        district: '-',
+        city: '-',
+        state: '-',
+        code: '-'
+      }
     }
 
     function isRequestok() {
-      return ajax.readyState === 4 && ajax.status === 200;
+      return ajax.readyState === 4;
     }
 
     function getUrl() {
-      return replaceCep('https://viacep.com.br/ws/[CEP]/json/');
+      return replaceCep(
+        'http://apps.widenet.com.br/busca-cep/api/cep/[CEP].json');
     }
 
-    function clearCep () {
+    function clearCep() {
       return $inputCep.get()[0].value.replace(/\D/g, '')
     }
 
@@ -171,7 +115,17 @@
       $status.get()[0].textContent = messages[type];
     }
 
-    function replaceCep (message) {
-      return message.replace('[CEP]',clearCep());
+    function replaceCep(message) {
+      return message.replace('[CEP]', clearCep());
     }
-    })();
+    return {
+      getMessage : getMessage ,
+      replaceCep : replaceCep
+    };
+  }
+
+  window.app = app;
+  app();
+
+
+})(window.DOM);
